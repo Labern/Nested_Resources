8a47bfd89e6613829f5418dd3ac3de1f58776377
Labern <lukelabern@gmail.com>
2024-11-08 21:09:02 +0000

    Working comments, attached to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_08_203541) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_08_204054) do
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -19,4 +28,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_08_203541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "posts"
 end
